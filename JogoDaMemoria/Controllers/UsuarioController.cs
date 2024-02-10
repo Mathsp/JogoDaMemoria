e9118af548f563feac01b54ede50061f73ea6797
Matheus Paes <matheushspaessilva@gmail.com>
2024-02-10 13:45:55 -0300

    Criação da controller usuario

---
+++ JogoDaMemoria/Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JogoDaMemoria.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public bool AutenticaUsuario(string userName, string Senha)
+        {
+            return true;
+        }
+
+        [HttpPost]
+        public void CadastroUsuario(string email, string userName, string nome, string senha, DateTime dataNascimento)
+        {
+            
+        }
+    }
+}
